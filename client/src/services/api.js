59caa97ff4c124810a65f892347e7725b69c7ca7
Srinethi Prakash <moonwheels14@gmail.com>
2026-04-22 18:53:05 +0530

    deploy frontend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:12000/api';
+console.log("API URL:", API_URL);
 
 const api = axios.create({
   baseURL: API_URL,
